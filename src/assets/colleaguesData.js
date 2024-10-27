f928c11ce248303e1f5112a51c052c66386e9b19
Takács Martin <originalmartin97@gmail.com>
2024-10-27 17:54:51 +0100

    colleagues now mapped by data

---
+++ src/assets/colleaguesData.js
@@ -0,0 +1,39 @@
+// src/assets/colleaguesData.js
+import mkJulia from './mkJulia.jpg'
+import nkBenedek from './nkBenedek.jpg'
+
+const colleaguesData = [
+  {
+    id: 1,
+    name: 'Mészárosné Kocsi Júlia',
+    shortDescription: '36 éves vagyok. Orvoslaboratóriumi analitikus a végzettségem és 13 éve dolgozom az egészségügyben...',
+    longDescription: `36 éves vagyok. Orvoslaboratóriumi analitikus a végzettségem és 13 éve 
+                      dolgozom az egészségügyben.
+                      A Metlife csapatát csak pár hónapja erősítem. A biztosítást azért választottam, mert látom a sok
+                      állami rendszerben eltöltött évemből és tapasztalatomból, mennyire fontos az öngondoskodás.
+                      Bízom benne, hogy a szaktudásommal hozzájárulhatok a sikerünkhöz és minél több
+                      ügyfelünknek segíthetünk.`,
+    image: mkJulia,
+  },
+  {
+    id: 2,
+    name: 'Nagy Benedek Károly',
+    shortDescription: `Nagy Benedek Károly vagyok, jogászhallgató, és a csapat legfiatalabb tagja. A biztosítások
+                       területén újnak számítok, azonban kollégáimnak köszönhetően hamar felvettem a fonalat és
+                       napról napra azon vagyok, hogy egyre jobban megismerjem ezt a területet. Véleményem szerint
+                       manapság minden embernek szüksége van egy biztosításra, amely a nehéz helyzetekben
+                       segítséget nyújt. Célom, hogy az ügyféllel közösen egy olyan valós segítséget nyújtó biztosítást
+                       állítsunk össze, amely az ügyfél elégedettségét szolgálja és biztonságban tudhassa magát és
+                       családtagjait.`,
+    longDescription: `Nagy Benedek Károly vagyok, jogászhallgató, és a csapat legfiatalabb tagja. A biztosítások
+                      területén újnak számítok, azonban kollégáimnak köszönhetően hamar felvettem a fonalat és
+                      napról napra azon vagyok, hogy egyre jobban megismerjem ezt a területet. Véleményem szerint
+                      manapság minden embernek szüksége van egy biztosításra, amely a nehéz helyzetekben
+                      segítséget nyújt. Célom, hogy az ügyféllel közösen egy olyan valós segítséget nyújtó biztosítást
+                      állítsunk össze, amely az ügyfél elégedettségét szolgálja és biztonságban tudhassa magát és
+                      családtagjait.`,
+    image: nkBenedek,
+  },
+]
+
+export default colleaguesData

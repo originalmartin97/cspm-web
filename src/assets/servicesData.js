eae6417c855b107a3cc1ace4127254eba69c2310
Takács Martin <originalmartin97@gmail.com>
2024-11-10 16:35:38 +0100

    services to Accordion initialized

---
+++ src/assets/servicesData.js
@@ -0,0 +1,50 @@
+
+
+
+const servicesData = [
+    {
+        id: 1,
+        title: 'Baleset és betegség biztosítások magánszemélyeknek és vállalkozásoknak',
+        description: `Egy váratlan esemény terveinket hamar keresztülhúzhatja.
+
+                        Mi az, ami Önnek a legjobban számít?
+                        • Egészséges gyermek születése?
+                        • A lakás törlesztő fizetése?
+                        • A család jövedelmének biztosítása?
+
+                        Családfenntartóként az optimális biztonság az éves jövedelem 10-szeresét jelenti. Ez
+                        ahhoz kell, hogy jövedelem megszűnés esetén is Ön fenn tudja tartani családja
+                        megszokott életszínvonalát.
+                        Céljai megvalósításakor Ön számol lehetséges jövedelemkieséssel?
+                        Mi segítünk!
+                        Célunk a család jövőjének és a mindennapok anyagi biztonságának biztosítása.
+                        Ez egy megtérülő biztonság
+
+                        Váratlan egészségügyi helyzetekre egy kiszámítható anyagi támogatást biztosítunk. Egyedi
+                        megoldások elérhetőek a családi életszínvonal magas szintű fenntartásához egy betegség,
+                        baleset bekövetkezése után is.
+
+                        Az alábbiakban felsorolt 9 fő termékünk 33 különböző fedezeti szinten választható az
+                        igényeinknek és/vagy félelmeinknek megfelelően:
+
+                        • haláleseti biztosítások
+                        • kritikus betegségekre szóló biztosítások
+                        • rokkantsági biztosítások
+                        • kórházi napi térítések
+                        • műtéti térítések
+                        • csonttörés és égési sérülésre szóló biztosítás
+                        • cukorbetegség súlyos szövődményeire szóló
+                        • biztosítás
+                        • járadék szolgáltatások
+                        • asszisztencia szolgáltatások (24/7 orvosi call center, második orvosi szakvélemény,
+                        szakorvosi vizsgálat megszervezése)
+
+                        Mindannyian ki vagyunk téve a jövőbeli események bizonytalanságának és különböző veszélyeknek.
+                        Mivel a balesetek és betegségek, a tragikus események szinte mindig anyagi veszteséggel, jövedelemkieséssel,
+                        sokszor komoly kiadásokkal is járnak, így érdemes felkészülni ezekre a nehéz helyzetekre.
+                        
+                        A MetProtect biztosítással megteremtheti a jövőjének biztonságát és mindennapjainak nyugalmát.`,
+    },
+]
+
+export default servicesData

ec6d62181eda6fa8e1580b277727df46819bef1e
Takács Martin <originalmartin97@gmail.com>
2024-12-05 13:09:34 +0100

    update to beta release

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -9,10 +9,21 @@ const BottomBar = () => {
           backgroundColor: 'rgb(0, 0, 0, 0.88)',
           alignItems: 'center',
           color: 'white',
+          textAlign: 'center',
         }}
       >
-        A kockazatokrol es mellekhatasokrol olvassa el a betegtajekoztatojat, vagy kerdezze meg kezeloorvosat vagy gyogyszereszetet.
-        A weboldalon szereplo informaciok nem helyettesitik az orvosi tanacsadasokat. Az oldal hasznalata sajat felelossegere tortenik.
+        A weboldalon található információk tájékoztató jellegűek és nem minősülnek szakmai tanácsadásnak.
+        A weboldal üzemeltetője nem vállal felelősséget az információk pontosságáért, teljességéért vagy aktualitásáért.
+        A weboldal használatával Ön elfogadja a jelen jogi nyilatkozatot.<br/>
+        <br/>
+        A weboldalon található tartalom, beleértve a szövegeket, képeket, videókat és egyéb anyagokat, szerzői jogi védelem alatt
+        állnak. Az engedély nélküli másolás, terjesztés vagy egyéb felhasználás tilos.<br/>
+        <br/>
+        A weboldalon található linkek más weboldalakra mutathatnak, amelyekért a weboldal üzemeltetője nem vállal felelősséget.
+        A linkek követésével Ön elhagyja a weboldalt, és egy harmadik fél weboldalára kerül.<br/>
+        <br/>
+        A jelen jogi nyilatkozat bármikor módosítható. Kérjük, rendszeresen látogassa meg ezt az oldalt, hogy tájékozódjon a
+        legfrissebb változásokról.
       </MyTypography>
   )
 }

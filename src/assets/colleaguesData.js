c7421173c17c6d0fed892c7eb1280eb75e427ab4
Takacs Martin <originalmartin97@gmail.com>
2025-06-18 18:09:11 +0000

    Add Varga Dávid to colleagues data and include associated image

--- src/assets/colleaguesData.js
+++ src/assets/colleaguesData.js
@@ -1,24 +1,31 @@
 // src/assets/colleaguesData.js
-import mkJulia from './mkJulia.jpg'
 import nkBenedek from './nkBenedek.jpg'
 import szfErzsebet from './szfErzsebet.jpg'
 import gkIlona from './gkIlona.jpg'
+import vDavid from './vDavid.jpeg'
 
 
 const colleaguesData = [
-/*  {
-    id: 3,
-    name: 'Mészárosné Kocsi Júlia',
-    shortDescription: '36 éves vagyok. Orvoslaboratóriumi analitikus a végzettségem és 13 éve dolgozom az egészségügyben...',
-    longDescription: `36 éves vagyok. Orvoslaboratóriumi analitikus a végzettségem és 13 éve dolgozom az egészségügyben. A Metlife csapatát csak pár hónapja erősítem. A biztosítást azért választottam, mert látom a sok állami rendszerben eltöltött évemből és tapasztalatomból, mennyire fontos az öngondoskodás. Bízom benne, hogy a szaktudásommal hozzájárulhatok a sikerünkhöz és minél több ügyfelünknek segíthetünk.`,
-    image: mkJulia,
-  },
-*/
   {
-    id: 5,
+    id: 4,
+    name: 'Varga Dávid',
+    shortDescription: '...azért választottam a pénzügyi területet, mert szeretnék valódi értéket adni az embereknek...',
+    longDescription: `
+                      Fiatal szakemberként azért választottam a pénzügyi területet, mert szeretnék valódi értéket adni az embereknek – legyen szó biztosításról, megtakarításról vagy egy hosszú távon működő pénzügyi stratégiáról.
+                      Fontos számomra, hogy az ügyfeleim ne érezzék magukat egyedül a döntéseikben. Hiszem, hogy megfelelő tájékoztatással és személyre szabott megoldásokkal mindenki képes lehet biztonságban tudni saját és családja jövőjét.
+                      A pénzügyi döntések nem egyik napról a másikra születnek – de a legjobb döntések mindig egy jó beszélgetéssel kezdődnek.
+                      `,
+    image: vDavid,
+  },
+
+  {
+    id: 3,
     name: 'Nagy Benedek Károly',
     shortDescription: `Nagy Benedek Károly vagyok, jogászhallgató, és a csapat legfiatalabb tagja. A biztosítások területén újnak számítok, azonban kollégáimnak köszönhetően...`,
-    longDescription: `Nagy Benedek Károly vagyok, jogászhallgató, és a csapat legfiatalabb tagja. A biztosítások területén újnak számítok, azonban kollégáimnak köszönhetően hamar felvettem a fonalat és napról napra azon vagyok, hogy egyre jobban megismerjem ezt a területet. Véleményem szerint manapság minden embernek szüksége van egy biztosításra, amely a nehéz helyzetekben segítséget nyújt. Célom, hogy az ügyféllel közösen egy olyan valós segítséget nyújtó biztosítást állítsunk össze, amely az ügyfél elégedettségét szolgálja és biztonságban tudhassa magát és családtagjait.`,
+    longDescription: `Nagy Benedek Károly vagyok, jogászhallgató, és a csapat legfiatalabb tagja.
+                      A biztosítások területén újnak számítok, azonban kollégáimnak köszönhetően hamar felvettem a fonalat és napról napra azon vagyok, hogy egyre jobban megismerjem ezt a területet.
+                      Véleményem szerint manapság minden embernek szüksége van egy biztosításra, amely a nehéz helyzetekben segítséget nyújt.
+                      Célom, hogy az ügyféllel közösen egy olyan valós segítséget nyújtó biztosítást állítsunk össze, amely az ügyfél elégedettségét szolgálja és biztonságban tudhassa magát és családtagjait.`,
     image: nkBenedek,
   },
   {
